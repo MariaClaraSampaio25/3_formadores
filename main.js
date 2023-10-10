1898c1e200cc39ff68f4035f514cb6ed7c6c93ed
Maria_Clara_Sampaio <127787008+MariaClaraSampaio25@users.noreply.github.com>
2023-10-10 20:35:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+//alert("Olá teste do formadores");
+
+function tocaSomRisada (){
+    document.querySelector("#som_tecla_risada").play();
+}
+
+document.querySelector(".tecla_risada").onclick = tocaSomRisada;
+
+function tocaSomGrilo (){
+    document.querySelector("#som_tecla_grilo").play();
+}
+
+document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
+
+function tocaSomErrado (){
+    document.querySelector("#som_tecla_errado").play();
+}
+
+document.querySelector(".tecla_errado").onclick = tocaSomErrado;
+
+function tocaSomAplauso (){
+    document.querySelector("#som_tecla_aplausos").play();
+}
+
+document.querySelector(".tecla_aplausos").onclick = tocaSomAplauso;
+
+function tocaSomDinheiro(){
+    document.querySelector("#som_tecla_dinheiro").play();
+}
+
+document.querySelector(".tecla_dinheiro").onclick = tocaSomDinheiro;
+
+function tocaSomTambores (){
+    document.querySelector("#som_tecla_tambores").play();
+}
+
+document.querySelector(".tecla_tambores").onclick = tocaSomTambores;
+
+function tocaSomTrombeta (){
+    document.querySelector("#som_tecla_trombeta").play();
+}
+
+document.querySelector(".tecla_trombeta").onclick = tocaSomTrombeta;
+
+function tocaSomVaia (){
+    document.querySelector("#som_tecla_vaia").play();
+}
+
+document.querySelector(".tecla_vaia").onclick = tocaSomVaia;
+
+function tocaSomVitoria (){
+    document.querySelector("#som_tecla_vitoria").play();
+}
+
+document.querySelector(".tecla_vitoria").onclick = tocaSomVitoria;
